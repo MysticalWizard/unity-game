541693997c73f156508bab907fa08803809bb972
MysticalWizard <mmaring0120@gmail.com>
2021-08-21 14:40:53 +0800

    Update Camera movements

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -23,12 +23,14 @@ public class CameraMovement : MonoBehaviour
         Vector3 pos = transform.position;
 
         if(Input.GetMouseButtonDown(0))
-            pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        {
+            camInitialPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
 
         if(Input.GetMouseButton(0))
         {
-            Vector3 camPos = camInitialPosition - cam.ScreenToWorldPoint(Input.mousePosition);
-            cam.transform.position += camPos;
+            Vector3 camDragDistance = camInitialPosition - cam.ScreenToWorldPoint(Input.mousePosition);
+            transform.position += camDragDistance;
         }
 
         if(Input.GetKey(KeyCode.W))
@@ -42,7 +44,7 @@ public class CameraMovement : MonoBehaviour
         }
 
         if(Input.GetKey(KeyCode.D))
-        {
+        {    
             pos.x += panSpeed * Time.deltaTime;
         }
 
@@ -53,7 +55,14 @@ public class CameraMovement : MonoBehaviour
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         cam.orthographicSize += scroll * scrollSpeed * 2 * Time.deltaTime;
-        if(cam.orthographicSize!>0)
+        if(cam.orthographicSize<1)
+        {
+            cam.orthographicSize = 1;
+        }
+        if(cam.orthographicSize>10)
+        {
+            cam.orthographicSize = 10;
+        }
         
         pos.y = Mathf.Clamp(pos.y, -panLimit.x, panLimit.x);
         pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
